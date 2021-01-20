ec732131cae803e7d3b4f5ff33449c6a8e5dec20
Jhonattas Henrique <jhonattas@soucriador.com>
2021-01-20 05:56:21 -0300

    update favicon

--- source/views/projects.php
+++ source/views/projects.php
@@ -1,6 +1,6 @@
 
 <!doctype html>
-<html lang="en">
+<html lang="pt-BR">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,13 +16,9 @@
     <?php require_once('components/viewport.php'); ?>
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
-    <meta name="theme-color" content="#7952b3">
+    <link rel="shortcut icon" href="https://blog.jhonattas.com/favicon.ico">
+    <!-- 32x32 -->
+    <link rel="shortcut icon" href="https://blog.jhonattas.com/favicon.png"><meta name="theme-color" content="#7952b3">
 
     <style>
       .bd-placeholder-img {
