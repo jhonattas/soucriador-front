bf4971528ee33362f2df2aeabde10cddbc2139c7
Jhonattas Henrique <jhonattas@soucriador.com>
2021-01-25 06:37:01 -0300

    update favicon

--- source/views/components/plugins/plugin-analytics.php
+++ source/views/components/plugins/plugin-analytics.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-41843022-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7CTTN65RME"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-41843022-1');
+  gtag('config', 'G-7CTTN65RME');
 </script>
