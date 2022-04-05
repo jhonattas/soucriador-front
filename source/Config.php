9f663240a45f6c131caba166612c3fc419043353
Jhonattas Henrique <jhonattas.goncalves@gmail.com>
2022-04-04 21:05:08 -0300

    atualiza o config

--- source/Config.php
+++ source/Config.php
@@ -2,7 +2,7 @@
 if(getenv('mode') == 'development'){
   define("URL_BASE", getenv('URL_BASE'));
   define("API", "http://localhost:3458/");
-  //define("API", "https://api.soucriador.com/");
+
 } else {
   //define("URL_BASE", "http://localhost/soucriador/front/");
   define("URL_BASE", "https://soucriador.com/");
